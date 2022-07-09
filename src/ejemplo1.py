976251544ab1b55ac3ab2a5c345f28bc4c731e99
jhocaba <srjhonst@hotmail.com>
2022-07-09 13:17:33 +0200

    Pagina de ejemplo

--- src/ejemplo1.py
+++ src/ejemplo1.py
@@ -1,3 +1,49 @@
-#Primer Ejemplo de Web Scraping con Python
+"""
+_______________________________________________________________________________________________________________________________
+                                    
+                                    EJEMPLO #1 DE WEB SCRAPING CON PYTHON --- BEAUTIFUL SOUP
+_______________________________________________________________________________________________________________________________
 
-web = [1,2,3,4]
+                                                __________________
+
+                                                TIPOS DE ELEMENTOS
+                                                __________________
+
+                                         Tag / Atributo / NavigableString
+TAG:
+    - Acceso mediante su nombre
+    - Pueden contener atributos
+        -> Acceso como si fuera un diccionario
+        -> Atributo attrs
+    - Propiedad string
+"""
+
+from bs4 import BeautifulSoup
+
+
+# Abrimos el fichero html que vamos a utilizar
+with open('index.html') as file:
+    html = file.read()
+
+"""
+Peticion con 'request' a una cierta pagina
+r = requests.get('https://webPage.com')
+"""
+
+# Nodo raiz del arbol de objetos definido a partir del documento HTML
+soup = BeautifulSoup(html, 'lxml')
+
+# A partir del nodo raiz, se realizan consultas para obtener los datos que realmente queremos utilizar
+
+# Acceso a la etiqueta titulo
+print(soup.title)
+
+# Acceso al encabezado h1
+print(soup.h1.string)
+
+# Acceso al primer enlace
+print(soup.a['href']) # Como si fuese un diccionario
+
+# Navegacion a traves de los nodos
+print(soup.div.div.div) #¡¡¡PROBLEMA: de esta manera solo recuperamos el primer nodo del arbol!!!
+
